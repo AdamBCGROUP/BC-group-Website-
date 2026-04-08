097623fe1fb0fd48eca8b7797c1e38b455a3a4b4
AdamBCGROUP <adampgredshaw@icloud.com>
2026-04-08 15:46:34 +1200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Mobile nav toggle
+const toggle = document.getElementById('navToggle');
+const links  = document.getElementById('navLinks');
+
+if (toggle && links) {
+  toggle.addEventListener('click', () => {
+    links.classList.toggle('open');
+  });
+
+  // Close menu when a link is clicked
+  links.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => links.classList.remove('open'));
+  });
+}
